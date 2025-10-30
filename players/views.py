3b2af44e8e80e1ba4454d0cd739c688c92238948
Nathan Harris <nharrisanalyst@gmail.com>
2025-10-29 17:46:32 -0700

    refactored title to use data

--- players/views.py
+++ players/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
+## data for the main site 
+main_context = {"title":"NBA MVP Predictor"}
+
 def main(request):
     return render(
         request,
-        'main.html'
+        'main.html',
+        main_context
     )
